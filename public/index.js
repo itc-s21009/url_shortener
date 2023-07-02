ea5c3d06d8af8e3e5960cb1f3fc7b09ee2e1aa9d
itc-s21009 <s21009@std.it-college.ac.jp>
2023-07-02 20:56:52 +0900

    生成履歴を出すようにした

--- public/index.js
+++ public/index.js
@@ -2,28 +2,65 @@ const root = ReactDOM.createRoot(document.getElementById('app'));
 
 const App = () => {
     const [text, setText] = React.useState('')
+    const [history, setHistory] = React.useState([])
     const isDisabled = !(text.startsWith('http://') || text.startsWith('https://'))
     const getDisabledClass = () => isDisabled ? 'disabled' : ''
     const handleInput = (e) => setText(e.target.value)
     const onSubmit = () => {
-        console.log('post')
         axios.post('/', {
             text: text
         }).then((r) => {
-            console.log(r)
+            const alias = r.data
+            const data = {
+                url: text,
+                alias: alias
+            }
+            if (!history.includes(data)) {
+                setHistory([...history, data])
+            }
         })
+    }
+    const ResultArea = () => {
+        if (history.length <= 0) {
+            return <></>
+        }
+        return (
+            <>
+                <table className={"table table-hover"}>
+                    <thead>
+                    <tr>
+                        <th>URL</th>
+                        <th>エイリアス</th>
+                        <th>コピー</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        history.map(h =>
+                            <tr key={h.url}>
+                                <td>{h.url}</td>
+                                <td>{h.alias}</td>
+                                <td>
+                                    <button className={`ms-2 btn btn-outline-primary text-nowrap`}>コピー</button>
+                                </td>
+                            </tr>
+                        )
+                    }
+                    </tbody>
+                </table>
+            </>
+        )
     }
     return (
         <>
-            <form action="/" method="post">
-                <div className="d-flex">
-                    <input type="text" name="text" id="url"
-                           placeholder="URL" className="form-control" onChange={(e) => handleInput(e)}/>
-                    <button className={`ms-2 btn btn-primary ${getDisabledClass()} text-nowrap`}
-                            onClick={() => onSubmit()}>短縮する
-                    </button>
-                </div>
-            </form>
+            <div className="d-flex">
+                <input type="text" name="text" id="url"
+                       placeholder="URL" className="form-control" onChange={(e) => handleInput(e)}/>
+                <button className={`ms-2 btn btn-primary ${getDisabledClass()} text-nowrap`}
+                        onClick={() => onSubmit()}>短縮する
+                </button>
+            </div>
+            <ResultArea/>
         </>
     )
 }
