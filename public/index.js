2fc24477e4644378ef5bbc89bb7a131c78fe9e88
itc-s21009 <s21009@std.it-college.ac.jp>
2023-07-02 15:01:15 +0900

    http:// か https:// から始まる時だけボタンが押せるように変更

--- public/index.js
+++ public/index.js
@@ -1,14 +1,24 @@
 const root = ReactDOM.createRoot(document.getElementById('app'));
 
-const App = () =>
-    <>
-        <form action="/" method="post">
-            <div className="d-flex">
-                <input type="text" name="text" id="url" placeholder="URL" className="form-control"/>
-                <button className="ms-2 btn btn-primary disabled text-nowrap">短縮する</button>
-            </div>
-        </form>
-    </>
+const App = () => {
+    const [disabled, setDisabled] = React.useState(true)
+    const getDisabledClass = () => disabled ? 'disabled' : ''
+    const handleInput = (e) => {
+        const text = e.target.value
+        setDisabled(!(text.startsWith('http://') || text.startsWith('https://')))
+    }
+    return (
+        <>
+            <form action="/" method="post">
+                <div className="d-flex">
+                    <input type="text" name="text" id="url"
+                           placeholder="URL" className="form-control" onChange={(e) => handleInput(e)}/>
+                    <button className={`ms-2 btn btn-primary ${getDisabledClass()} text-nowrap`}>短縮する</button>
+                </div>
+            </form>
+        </>
+    )
+}
 
 
-root.render(<App />);
+root.render(<App/>);
